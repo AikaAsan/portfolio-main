f2727e64683091d81a684b259df16ff1776d5aeb
aika <aiganysha@gmail.com>
2021-02-09 12:32:16 -0600

    switched folders

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const navSlide = function() {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-link');
     const navLinks = document.querySelectorAll('.nav-link li');
+    
+    // Toggle Nav ES6 version 
+    // burger.addEventListener('click', ()=> {
+    //     nav.classList.toggle('nav-active');
+    // });
 
-
-      burger.addEventListener('click', function(){
+    burger.addEventListener('click', function(){
     //Toggle Nav
         nav.classList.toggle('nav-active');
 
@@ -20,54 +24,5 @@ const navSlide = function() {
     });
 
 }
+
 navSlide();
-
-
-// form submission success/error 
-
- window.addEventListener("DOMContentLoaded", function() {
-
-    // get the form elements defined in your form HTML above
-    
-    var form = document.getElementById("my-form");
-    // var button = document.getElementById("my-form-button");
-    var status = document.getElementById("status");
-
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      status.classList.add('success')
-      status.innerHTML = "Thanks!";
-    }
-
-    function error() {
-        status.classList.add('error')
-      status.innerHTML = "Oops! There was a problem.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
-
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
